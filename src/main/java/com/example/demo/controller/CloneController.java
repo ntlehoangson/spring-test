b3ae0d42e0225f49ffd1d0cbb069ea739ea04d82
hoangson <ntle.hoangson@gmail.com>
2024-06-03 17:57:13 +0700

    clone1

--- src/main/java/com/example/demo/controller/CloneController.java
+++ src/main/java/com/example/demo/controller/CloneController.java
@@ -2,6 +2,6 @@ package com.example.demo.controller;
 
 public class CloneController {
     public static void main(String[] args) {
-        System.out.println("clone");
+        System.out.println("clone1");
     }
 }
