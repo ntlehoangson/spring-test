60882d70b4d3a24781a14164f17409faea52d968
hoangson <ntle.hoangson@gmail.com>
2024-06-03 17:55:39 +0700

    Revert "cm3"
    This reverts commit e12e2b6c

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestController {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(5);
+        System.out.println(4);
     }
 }
