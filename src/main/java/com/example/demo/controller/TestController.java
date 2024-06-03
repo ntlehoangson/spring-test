ba44b79d25c1209affbc37227f72fa97e9d37317
hoangson <ntle.hoangson@gmail.com>
2024-06-03 17:48:55 +0700

    cm1

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
