502476e1b0ce2c2d882c1a467a544fc937b2c4e2
hoangson <ntle.hoangson@gmail.com>
2024-06-03 17:49:08 +0700

    cm2

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestController {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(2);
     }
 }
